8a9e5f31fa125e4355ec7a172a602422e9ce178b
Theo Immi <timmi@c1r9s3.42lausanne.ch>
2025-04-03 11:50:30 +0200

    correction

--- include/minishell.h
+++ include/minishell.h
@@ -1,3 +1,11 @@
+#ifndef MINISHELL_H
+#define MINISHEL_H
+
 # include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 # include <unistd.h>
 # include <string.h>
+
+#endif
