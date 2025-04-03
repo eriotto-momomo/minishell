86aa0e84188636f590885ec1499cc80c8b760682
C4v3d <theoimmi@pm.me>
2025-04-03 12:42:09 +0200

    Ajout des fonction de prompt

--- lib/libft/ft_strjoin.c
+++ lib/libft/ft_strjoin.c
@@ -3,29 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emonacho <emonacho@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 11:52:28 by emonacho          #+#    #+#             */
-/*   Updated: 2025/01/14 20:24:24 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/04/03 12:38:37 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t		s1_len;
-	size_t		s2_len;
-	char		*new_s;
+	int		i;
+	int		j;
+	size_t	len;
+	char	*ptr;
 
-	s1_len = ft_strlen (s1);
-	s2_len = ft_strlen (s2);
-	new_s = malloc(sizeof(char) * (s1_len + s2_len) + 1);
-	if (!new_s)
-		return (0);
-	ft_memcpy(new_s, s1, s1_len);
-	ft_memcpy((new_s + s1_len), s2, s2_len);
-	new_s[s1_len + s2_len] = '\0';
-	free(s1);
-	return (new_s);
+	i = 0;
+	j = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
+	ptr = malloc((len * sizeof(char)) + 1);
+	if (!ptr)
+		return (NULL);
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
+
+// char	*ft_strjoin(char *s1, char *s2)
+// {
+// 	size_t		s1_len;
+// 	size_t		s2_len;
+// 	char		*new_s;
+
+// 	s1_len = ft_strlen (s1);
+// 	s2_len = ft_strlen (s2);
+// 	new_s = malloc(sizeof(char) * (s1_len + s2_len) + 1);
+// 	if (!new_s)
+// 		return (0);
+// 	ft_memcpy(new_s, s1, s1_len);
+// 	ft_memcpy((new_s + s1_len), s2, s2_len);
+// 	new_s[s1_len + s2_len] = '\0';
+// 	free(s1);
+// 	return (new_s);
+// }
