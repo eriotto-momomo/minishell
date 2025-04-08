7f05bda02c6d61b2e7c05fd33986618b85e68c43
Theo Immi <timmi@c2r8s3.42lausanne.ch>
2025-04-08 15:26:53 +0200

    Break

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 15:19:48 by timmi             #+#    #+#             */
-/*   Updated: 2025/04/04 15:31:38 by timmi            ###   ########.fr       */
+/*   Updated: 2025/04/08 15:12:59 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,29 @@
 # include <string.h>
 # include "../lib/libft/libft.h"
 
+// Prompt par default utiliser si le custom prompt fail
 # define DEFAULT_PROMPT "minishell-1.0$ "
+// Char set de caractere qui peuvent separer des token dans une commande
+# define SEPARATORS "|\"\'<>"
+/*
+ Attribution d'une valeur constante pour chaque token
+ - WORD =	0
+ - PIPE =	1
+ - REDIR =	2
+*/
+typedef enum e_types
+{
+	WORD,
+	PIPE,
+	REDIR
+}	t_types;
+
+typedef struct s_list
+{
+	char			*value;
+	t_types			type;
+	struct s_list	*next;
+}					t_list;
 
 /*			PROMPT			*/
 void	prompt_loop(char *prompt);
