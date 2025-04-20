5d096b2b9bc11f28665f13df5684335b78bd26ac
eriotto-momomo <eliot.monachon@proton.me>
2025-04-20 22:10:53 +0200

    WIP👷‍♂️... hesite pas a mettre  en commentaire dans main.c si jamais! ;)

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,15 +6,18 @@
 /*   By: emonacho <emonacho@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 20:39:40 by emonacho          #+#    #+#             */
-/*   Updated: 2025/04/19 19:54:31 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/04/20 22:07:44 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-/*
-t_ast	parse_tokens(t_list *head)
+// ⚠️👷‍♂️ Travail en cours, fonctions probablement à modifier
+
+t_ast	*parse_tokens(t_list **head)
 {
-	...
+	t_ast	*ast;
+
+	ast = parse_line(head);
+	return (ast);
 }
-*/
