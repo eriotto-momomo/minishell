f8b097dace7e76cd79e4ac22b8954407551ba782
Eliot Monachon <emonacho@c1r9s1.42lausanne.ch>
2025-04-25 22:27:47 +0200

    WIP parser

--- include/struct.h
+++ include/struct.h
@@ -6,7 +6,7 @@
 /*   By: emonacho <emonacho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 10:41:49 by timmi             #+#    #+#             */
-/*   Updated: 2025/04/24 16:43:32 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/04/25 22:26:44 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,9 @@ typedef enum e_types
 	PIPE,
 } t_types;
 
-// ⚠️👷‍♂️ Travail en cours, fonctions et structures probablement à modifier
-
-// Adapté de: https://keleshev.com/abstract-syntax-tree-an-example-in-c/
 typedef enum	e_tag
 {
-	AST_BLOCK,
 	AST_PIPE,
-	AST_LINE,
 	AST_REDIR,
 	AST_EXEC
 } 				t_tag;
@@ -49,11 +44,9 @@ typedef struct s_ast t_ast;
 
 typedef union	u_data
 {
-	struct { t_ast *left; t_ast *right; } ast_block;
-	struct { t_ast *left; t_ast *right; } ast_pipe;
-	struct { t_ast *left; t_ast *right; } ast_line;
-	struct { t_ast *left; char *filename; int mode; } ast_redir;
-	struct { int argc; char **argv; } ast_exec;
+	struct { t_ast *parent; t_ast *left; t_ast *right; } ast_pipe;
+	struct { t_ast *parent; t_ast *left; char *filename; int mode; } ast_redir;
+	struct { t_ast *parent; int argc; char **argv; } ast_exec;
 }				t_data;
 
 typedef struct	s_ast
