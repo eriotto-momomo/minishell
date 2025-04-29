abb033a3519ba442a72b90a25e54b3f1aa2a9303
Theo <theoimmi@gmail.com>
2025-04-29 15:33:21 +0200

    it is not working

--- src/builtin/ft_echo.c
+++ src/builtin/ft_echo.c
@@ -6,34 +6,66 @@
 /*   By: c4v3d <c4v3d@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 10:08:35 by c4v3d             #+#    #+#             */
-/*   Updated: 2025/04/29 11:23:23 by c4v3d            ###   ########.fr       */
+/*   Updated: 2025/04/29 15:32:49 by c4v3d            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int ft_echo(char **args, int fd_out)
+static size_t	len_calc(char **tab)
 {
-	char 	*temp;
-	int 	ret;
-	int 	i;
-	int 	append;
+	size_t	total_len;
+	int		i;
 
 	i = 1;
-	ret = 0;
-	append = 1;
-	if (ft_memcmp(args[i], "-n", 2))
-		append = 0;
-	while (args[i])
+	total_len = 0;
+	while (tab[i])
 	{
-		
-		temp = ft_strjoin(args[i], " ");
-		ret = write(fd_out, temp, ft_strlen(temp));
-		if (ret == -1)
-			return (ret);
-		free(temp);
+		total_len += ft_strlen(tab[i]);
+		if (tab[i + 1])
+			total_len += 1;
 		i++;
 	}
-	write(fd_out, "\n", 1);
-	return (ret);
+	return (total_len);
+}
+
+static char	*tabtos(char **tab)
+{
+	char 	*ptr;
+	int		i;
+	size_t	total_len;
+	char	*final_str;
+	int		len;
+	
+	i = 1;
+	len = 0;
+	total_len = len_calc(tab);
+	final_str = malloc(total_len + 1);
+	if (!final_str)
+		return (NULL);
+	while (tab[i])
+	{
+		len = ft_strlen(tab[i]);
+		ft_memcpy(ptr, tab[i], len);
+		ptr += len;
+		if (tab[i + 1])
+		{
+			*ptr = ' ';
+			ptr++;
+		}
+		i++;
+	}
+	*ptr = '\0';
+	printf("str :%s\n", final_str);
+	return (final_str);
+}
+
+int	ft_echo(char **args, int fd_out)
+{
+	char	*str;
+
+	if (args[1])
+		str = tabtos(args);
+	write(fd_out, str, ft_strlen(str));
+	return (1);
 }
