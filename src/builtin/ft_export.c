2961ed53c27d4e97f3c984e34e2cb0f1d121d601
Theo Immi <timmi@c1r9s6.42lausanne.ch>
2025-05-08 20:53:46 +0200

    yes !

--- src/builtin/ft_export.c
+++ src/builtin/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 17:41:13 by timmi             #+#    #+#             */
-/*   Updated: 2025/05/08 18:54:22 by timmi            ###   ########.fr       */
+/*   Updated: 2025/05/08 20:53:16 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ static void	treat_var(t_env *env, char *arg)
 	char	*value;
 	t_env	*ptr;
 	
-	if (ft_strchr(arg, '='))
+	if (!ft_strchr(arg, '='))
 		return ;
 	name = get_name(arg);
-	value = get_name(arg);
+	value = get_value(arg);
 	ptr = env;
 	while (ptr)
 	{
 		if (ft_strncmp(ptr->name, name, ft_strlen(name)) == 0)
 		{
 			w_free((void **)&(ptr->value));
-			ptr->name = value;
+			ptr->value = ft_strdup(value);
 			return ;
 		}
 		ptr = ptr->next;
@@ -43,6 +43,7 @@ int	ft_export(t_shell *s)
 
 	if (s->root_node->data.ast_exec.argc == 1)
 		return (0);
+	i = 1;
 	args = s->root_node->data.ast_exec.argv;
 	while (args[i])
 		treat_var(s->env_list, args[i++]);
