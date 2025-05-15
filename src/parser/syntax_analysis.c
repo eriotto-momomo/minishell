7018e745ef578961ab377f16fef202d3cde5a9b3
Theo Immi <timmi@c1r9s3.42lausanne.ch>
2025-05-15 16:51:02 +0200

    WIP analyusis

--- src/parser/syntax_analysis.c
+++ src/parser/syntax_analysis.c
@@ -6,7 +6,7 @@
 /*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 14:20:33 by timmi             #+#    #+#             */
-/*   Updated: 2025/05/15 15:53:54 by timmi            ###   ########.fr       */
+/*   Updated: 2025/05/15 16:14:55 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,64 +14,50 @@
 
 static int	quote_check(t_list *tok)
 {
-	int		err;
-
-	err = 0;
-	if (tok->data[0] == "\'" || tok->data[0] == '\"')
+	if (tok->data[0] == '\'' || tok->data[0] == '\"')
 	{
 		if (tok->data[0] == '\'' && tok->data[ft_strlen(tok->data)] != '\'')
-			err = UNMATCHED_QUOTE;
+			return (1);
 		else if (tok->data[0] == '\"' && tok->data[ft_strlen(tok->data)] != '\"')
-			err = UNMATCHED_QUOTE;
+			return (1);
 	}
-	return (err);
+	return (0);
 }
 
-static int	pipe_check(t_list *tok)
+static int	syntax_checker(t_list *tok)
 {
-	int	err;
-	
-	err = 0;
-	if (tok->type == PIPE)
+	if (tok->data[0] == '\"' || tok->data[0] == '\'')
 	{
-		if (!tok->prev || !tok->next || tok->prev->type != WORD)
-			err = UNEXPECTED_TOK;
-		else if (tok->next->type == PIPE)
-			err = 2; // minishell: Does not handle `||\n
+		if (quote_check(tok))
+			return (UNMATCHED_QUOTE);
+		else
+			return (0);
 	}
-	return (err);
-}
-
-static int	redir_check(t_list *tok)
-{
-	int	err;
-	
-	err = 0;
 	if (tok->type == OUT_REDIR || tok->type == IN_REDIR
 		|| tok->type == HERE_DOC || tok->type == APP_OUT_REDIR)
 	{
 		if (!tok->next)
-			err = UNEXPECTED_TOK;
+			return (UNEXPECTED_TOK);
 	}
-	return (err);
+	if (tok->type == PIPE)
+	{
+		if (!tok->prev || !tok->next || tok->prev->type != WORD)
+			return (UNEXPECTED_TOK);
+		else if (tok->next->type == PIPE)
+			return (2);
+	}
+	return (0);
 }
 
-static int	syntax_checker(t_list *tok, int *err)
+int	syntax_analysis(t_list *current_tok)
 {
-	
-}
-
-int	syntax_analysis(t_shell *s)
-{
-	t_list	*current_tok;
 	int		err;
 
-	current_tok = s->head;
 	while (current_tok)
 	{
-		syntax_checker(current_tok, &(s->err));
-		if (s->err)
-			return (0);
+		err = syntax_checker(current_tok);
+		if (err)
+			return (err);
 		current_tok = current_tok->next;
 	}
 	return (1);
