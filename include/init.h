47902d90f293c607944997232811ee6ffdd411d4
C4v3d <theoimmi@pm.me>
2025-05-21 14:51:43 +0200

    clean

--- include/init.h
+++ include/init.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emonacho <emonacho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 21:27:42 by timmi             #+#    #+#             */
-/*   Updated: 2025/05/16 19:18:58 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/05/21 14:50:05 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ typedef struct s_shell t_shell;
 t_env	*table_to_ll(char **env);
 char	*get_value(char *s);
 char	*get_name(char	*s);
-char	**ll_to_table(t_env *h_env);
 
 // Struct
 void init_shell(t_shell *s, char	**envp);
