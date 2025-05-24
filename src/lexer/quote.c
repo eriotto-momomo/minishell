a956d5fb911b8312d739954dfa85caec0a943a5e
Theo Immi <timmi@c1r9s3.42lausanne.ch>
2025-05-24 15:47:11 +0200

    added the process_quote fuction I need to adapt the tokenize function now

---
+++ src/lexer/quote.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quote.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/24 14:45:19 by timmi             #+#    #+#             */
+/*   Updated: 2025/05/24 15:45:22 by timmi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../../include/minishell.h"
+
+int process_quote(char **s)
+{
+	char *trimmed;
+	char *str;
+	size_t len;
+
+	str = *s;
+	len = ft_strlen(str);
+	if ((str[0] == '\'' && str[len - 1] == '\'') ||
+		(str[0] == '"' && str[len - 1] == '"'))
+	{
+		trimmed = ft_substr(str, 1, len - 2);
+		if (!trimmed)
+			return (0);
+		free(*s);
+		*s = trimmed;
+	}
+	return (1);
+}
