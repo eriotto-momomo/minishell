74d5cc767450bdd15c84b4378de66e98c183b9d9
Theo Immi <timmi@c1r9s3.42lausanne.ch>
2025-06-03 10:17:55 +0200

    WIP var expansion

--- include/expand.h
+++ include/expand.h
@@ -6,16 +6,13 @@
 /*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 14:54:52 by timmi             #+#    #+#             */
-/*   Updated: 2025/05/30 15:21:51 by timmi            ###   ########.fr       */
+/*   Updated: 2025/06/03 08:21:21 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXPAND_H
 # define EXPAND_H
 
-//UTILS
-int		is_in_env(t_env	*env, char *var);
-size_t	offset_calc(char *str);
 int		expand(t_env *env, char **str);
 
 // Word processing
