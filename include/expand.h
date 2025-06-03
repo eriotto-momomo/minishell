f962e593daa7fa2b8ecbacbbffb0a192ec380422
Theo Immi <timmi@c2r2s6.42lausanne.ch>
2025-06-03 14:08:34 +0200

    WIP expansion de merde

--- include/expand.h
+++ include/expand.h
@@ -6,14 +6,16 @@
 /*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 14:54:52 by timmi             #+#    #+#             */
-/*   Updated: 2025/06/03 11:37:01 by timmi            ###   ########.fr       */
+/*   Updated: 2025/06/03 12:55:59 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXPAND_H
 # define EXPAND_H
 
-int		expand(t_env *env, char *str);
+char	*expand(t_env *env, char *str);
+int		var_expansion(t_env * env, char **s);
+
 
 // Word processing
 int	string_processing(t_shell *s, char **args);
--- src/expand/word_processing.c
+++ src/expand/word_processing.c
@@ -6,7 +6,7 @@
 /*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 14:34:19 by timmi             #+#    #+#             */
-/*   Updated: 2025/06/03 11:36:32 by timmi            ###   ########.fr       */
+/*   Updated: 2025/06/03 12:55:09 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,7 @@ int	string_processing(t_shell *s, char **args)
 	{
 		if (ft_strchr(args[i], '$'))
 		{
-			if (!expand(s->env_list, args[i]))
+			if (!var_expansion(s->env_list, &args[i]))
 				return (0);
 		}
 		if (ft_strchr(args[i], '\'') || ft_strchr(args[i], '\"'))
