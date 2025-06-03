cbf6df1be2c3d6b72450429f53bea7c6833a8348
C4v3d <theoimmi@pm.me>
2025-06-03 11:52:15 +0200

    WIP expansion variable and quote removale

--- include/expand.h
+++ include/expand.h
@@ -6,14 +6,14 @@
 /*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 14:54:52 by timmi             #+#    #+#             */
-/*   Updated: 2025/06/03 08:21:21 by timmi            ###   ########.fr       */
+/*   Updated: 2025/06/03 11:37:01 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXPAND_H
 # define EXPAND_H
 
-int		expand(t_env *env, char **str);
+int		expand(t_env *env, char *str);
 
 // Word processing
 int	string_processing(t_shell *s, char **args);
