12a5a86580345a3ff511bebc715c4fc72fb25146
Theo Immi <timmi@c1r9s3.42lausanne.ch>
2025-06-05 12:07:35 +0200

    WIP correction de certaines allocation memoire non protegee

--- src/expand/var_expansion.c
+++ src/expand/var_expansion.c
@@ -6,76 +6,35 @@
 /*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 14:11:17 by timmi             #+#    #+#             */
-/*   Updated: 2025/06/03 14:07:10 by timmi            ###   ########.fr       */
+/*   Updated: 2025/06/05 12:04:38 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static char	*triple_join(char *s1, char *s2, char *s3)
-{
-	char	*tmp;
-	char	*ret;
-
-	if (!s1 || !s2 || !s3)
-		return (NULL);
-	tmp = ft_strjoin(s1, s2);
-	if (!tmp)
-		return (NULL);
-	free(s1);
-	free(s2);
-	ret = ft_strjoin(tmp, s3);
-	free(tmp);
-	free(s3);
-	if (!ret)
-		return (NULL);
-	return (ret);
-}
-
-static char	*get_var(t_env *env, char *s)
-{
-	int		end;
-	char	*var_name;
-	char	*var_val;
-
-	end = 0;
-	while (s[end] && (ft_isalnum(s[end])))
-		end++;
-	var_name = ft_substr(s, 0, end);
-	if (!var_name)
-		return (NULL);
-	var_val = ft_strdup(ft_getenv(env, var_name));
-	free(var_name);
-	if (var_val)
-		return (var_val);
-	free(var_val);
-	return (ft_strdup(""));
-}
-
 static int	attribute_value(t_env *env, char **str, int i)
 {
-	char	*value;
 	char	*prefix;
-	char	*post;
-	char	*ret_s;
+	char	*sufix;
 
-	value = get_var(env, *str + i + 1);
-	if (!value)
+	prefix = make_prefix(env, *str, i);
+	if (!prefix)
 		return (0);
-	prefix = ft_substr(*str, 0, i);
 	i++;
 	while ((*str)[i] && ft_isalnum((*str)[i]))
 		i++;
-	post = ft_substr(*str, i, ft_strlen(*str));
-	ret_s = triple_join(prefix, value, post);
-	if (!ret_s)
+	sufix = ft_substr(*str, i, ft_strlen(*str));
+	if (!sufix)
 	{
 		free(prefix);
-		free(post);
 		return (0);
 	}
 	free(*str);
-	*str = ret_s;
+	*str = ft_strjoin(prefix, sufix);
+	free(prefix);
+	free(sufix);
+	if (!*str)
+		return (0);
 	return (1);
 }
 
--- src/expand/var_expansion_utils.c
+++ src/expand/var_expansion_utils.c
@@ -6,7 +6,7 @@
 /*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 15:18:49 by timmi             #+#    #+#             */
-/*   Updated: 2025/06/03 10:33:10 by timmi            ###   ########.fr       */
+/*   Updated: 2025/06/05 12:06:00 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,44 @@ int	is_in_double_quote(char *s, int j)
 		i++;
 	}
 	return (in_double);
+}
+
+char	*make_prefix(t_env *env, char *str, int i)
+{
+	char	*value;
+	char	*prefix;
+	char	*ret;
+
+	value = get_var(env, str + i + 1);
+	if (!value)
+		return (NULL);
+	prefix = ft_substr(str, 0, i);
+	if (!prefix)
+	{
+		free(value);
+		return (NULL);
+	}
+	ret = ft_strjoin(prefix, value);
+	free(prefix);
+	free(value);
+	return (ret);
+}
+char	*get_var(t_env *env, char *s)
+{
+	int		end;
+	char	*var_name;
+	char	*var_val;
+
+	end = 0;
+	while (s[end] && (ft_isalnum(s[end])))
+		end++;
+	var_name = ft_substr(s, 0, end);
+	if (!var_name)
+		return (NULL);
+	var_val = ft_strdup(ft_getenv(env, var_name));
+	free(var_name);
+	if (var_val)
+		return (var_val);
+	free(var_val);
+	return (ft_strdup(""));
 }
