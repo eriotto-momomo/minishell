6b7e78a853664135b7b53b1980ea4de79018f356
Theo Immi <timmi@c3r5s1.42lausanne.ch>
2025-06-23 19:48:29 +0200

    WIP

--- src/init/init_shell.c
+++ src/init/init_shell.c
@@ -6,7 +6,7 @@
 /*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 13:03:30 by timmi             #+#    #+#             */
-/*   Updated: 2025/06/23 18:53:29 by timmi            ###   ########.fr       */
+/*   Updated: 2025/06/23 19:47:13 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ int	init_shell(t_shell *s, char	**envp)
 	s->env_list = table_to_ll(envp);
 	if (!s->env_list)
 		return (-1);
+	s->heredoc_tmp = ft_strdup(HEREDOC_FILE_PATH);
+	if (!s->heredoc_tmp)
+		return (print_error(&s->numerr, ENOMEM, "ft_strdup"));
 	s->ret_value = 0;
 	s->numerr = 0;
 	s->pipe_count = 0;
